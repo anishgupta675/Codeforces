fe26e9cf0fc34fc8343806a46a45b78323d4ed26
Anish Gupta <anishgupta675@gmail.com>
2022-07-13 23:28:01 +0530

    bin

--- Templates/CPP_Code_Template/src/main.cpp
+++ Templates/CPP_Code_Template/src/main.cpp
@@ -9,17 +9,15 @@ const int MAX_N = 1e5 + 1;
 const ll MOD = 1e9 + 7;
 const ll INF = 1e9;
 
-
-
 void solve() {
-
+    
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
     int tc = 1;
-    // cin >> tc;
+    cin >> tc;
     for (int t = 1; t <= tc; t++) {
         // cout << "Case #" << t  << ": ";
         solve();
