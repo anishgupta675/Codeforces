390ebdf6d8b0e42dce097e206c1e17d89fbbeaf9
Anish Gupta <anishgupta675@gmail.com>
2023-04-16 22:30:52 +0530

    CSES ProblemSet 3: Ferris Wheel

---
+++ CSES_ProblemSet/Ferris_Wheel/main.cpp
@@ -0,0 +1,56 @@
+/*
+Problem Details:
+    Contest Event Name: ?
+    Contest Event Link: ?
+    Problem Statement Label Number: ?
+    Problem Statement Code Number: ?
+    Problem Statement Link Number: ?
+*/
+
+/*
+Short explanation: ?
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ar array
+#define ll long long
+
+const int MAX_N = 1e5 + 1;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+
+int getGondolas(vector<int> &p, int n, int x) {
+    int i = 0, j = p.size() - 1, cnt = 0;
+    while(i <= j) {
+        if(p[i] + p[j] <= x) {
+            i++;
+            j--;
+        }
+        j--;
+        cnt++;
+    }
+    return cnt;
+}
+
+void solve() {
+    int n, x;
+    cin >> n >> x;
+    vector<int> p(n);
+    for(int i = 0; i < n; i++) cin >> p[i];
+    cout << getGondolas(p, n, x) << endl;
+    p.clear();
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t = 1;
+    // cin >> t;
+    while(t--) {
+        // cout << "Case #" << t  << ": ";
+        solve();
+    }
+}
