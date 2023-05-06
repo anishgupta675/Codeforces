0c1b847d6134f3509b411d7402a54c2204e8a7c1
Anish Gupta <anishgupta675@gmail.com>
2023-05-06 23:54:25 +0530

    CSES Searching and Sorting Problem C

--- CSES_ProblemSet/Ferris_Wheel/main.cpp
+++ CSES_ProblemSet/Ferris_Wheel/main.cpp
@@ -1,10 +1,10 @@
 /*
 Problem Details:
-    Contest Event Name: ?
-    Contest Event Link: ?
-    Problem Statement Label Number: ?
-    Problem Statement Code Number: ?
-    Problem Statement Link Number: ?
+    Contest Event Name: CSES Problem Set Sorting and Searching
+    Contest Event Link: https://codeforces.com/gym/102961
+    Problem Statement Label Number: C
+    Problem Statement Code Number: 102961C
+    Problem Statement Link Number: https://codeforces.com/gym/102961/problem/C
 */
 
 /*
