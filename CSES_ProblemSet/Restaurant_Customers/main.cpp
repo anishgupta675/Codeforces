629b03f0db498d7be07b187fdc87c4b0319b27b2
Anish Gupta <anishgupta675@gmail.com>
2023-05-06 23:55:54 +0530

    CSES Searching and Sorting Problem E

---
+++ CSES_ProblemSet/Restaurant_Customers/main.cpp
@@ -0,0 +1,60 @@
+/*
+Problem Details:
+    Contest Event Name: CSES Problem Set Sorting and Searching
+    Contest Event Link: https://codeforces.com/gym/102961
+    Problem Statement Label Number: E
+    Problem Statement Code Number: 102961E
+    Problem Statement Link Number: https://codeforces.com/gym/102961/problem/E
+*/
+
+/*
+Short explanation: ?
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ar array
+#define ll long long
+
+const int MAX_N = 1e5 + 1;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+
+pair<int, int> getBounds(vector<pair<int, int>> &customers) {
+    pair<int, int> res;
+    for(int i = 0; i < customers.size(); i++) {
+        res.first = min(customers[i].first, res.first);
+        res.second = max(customers[i].second, res.second);
+    }
+    return res;
+}
+
+int getCustomers(vector<pair<int, int>> &customers) {
+    int max_customers = 0;
+    pair<int, int> bounds = getBounds(customers);
+    vector<int> map(bounds.second - bounds.first);
+    for(int i = 0; i < customers.size(); i++) for(int j = customers[i].first; j <= customers[i].second; j++) map[j - bounds.first]++;
+    for(int i = 0; i < map.size(); i++) max_customers = max(map[i], max_customers);
+    return max_customers;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<pair<int, int>> customers(n);
+    for(int i = 0; i < n; i++) cin >> customers[i].first >> customers[i].second;
+    cout << getCustomers(customers) << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        // cout << "Case #" << t  << ": ";
+        solve();
+    }
+}
