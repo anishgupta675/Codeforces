fc9e95d18f62177414ff85e7ffbd03a9d57c040b
Anish Gupta <anishgupta675@gmail.com>
2023-10-09 21:30:46 +0530

    Anya and Smartphone

---
+++ Anya_and_Smartphone/main.cpp
@@ -0,0 +1,43 @@
+/*
+Problem Details:
+    Contest Event Name: Codeforces Round 293 (Div. 2)
+    Contest Event Link: https://codeforces.com/contest/518
+    Problem Statement Label Number: C
+    Problem Statement Code Number: 518C
+    Problem Statement Link Number: https://codeforces.com/problemset/problem/518/C
+*/
+
+/*
+Short explanation: ?
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ar array
+#define ll long long
+
+const int MAX_N = 1e5 + 1;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+
+void solve() {
+    int n, m, k;
+    cin >> n >> m >> k;
+    vector<int> a(n);
+    vector<int> b(m);
+    for(int i = 0; i < n; i++) cin >> a[i];
+    for(int i = 0; i < m; i++) cin >> b[i];
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        // cout << "Case #" << t  << ": ";
+        solve();
+    }
+}
