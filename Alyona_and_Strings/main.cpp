176193d8d21a84878bec6a8a4ccd22b4a2746d2d
Anish Gupta <anishgupta675@gmail.com>
2024-12-14 18:28:13 +0000

    Alyona and Strings

---
+++ Alyona_and_Strings/main.cpp
@@ -0,0 +1,38 @@
+/*
+Problem Details:
+    Contest Event Name: Codeforces Round 358 (Div. 2)
+    Contest Event Link: https://codeforces.com/contest/682
+    Problem Statement Label Number: D
+    Problem Statement Code Number: 682D
+    Problem Statement Link Number: https://codeforces.com/contest/682/problem/D
+*/
+
+/*
+Short explanation: ?
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ar array
+#define ll long long
+
+const int MAX_N = 1e5 + 1;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+
+void solve() {
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        // cout << "Case #" << t  << ": ";
+        solve();
+    }
+}
